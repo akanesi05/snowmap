ad109dc441bb14f3127e0b158fbac9ef5b0be0ef
mitsuiakane <akane.sumitomo@gmail.com>
2026-03-21 17:24:00 +0900

    feat: ユーザー登録機能を実装（自動ログイン処理追加）途中

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { signIn } from "next-auth/react";
+
+export default function Page() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const name = formData.get("name")?.toString();
+    const email = formData.get("email")?.toString();
+    const password = formData.get("password")?.toString();
+    if (!email || !password) {
+       alert("メールとパスワードを入力してください");
+       return;
+    }
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+       await signIn("credentials", {
+       email,
+       password,
+       redirect: true,
+       callbackUrl: "/", 
+  });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        ユーザ名
+        <input name="name" type="text" />
+      </label>
+
+      <label>
+        メールアドレス
+        <input name="email" type="email" />
+      </label>
+
+      <label>
+        パスワード
+        <input name="password" type="password" />
+      </label>
+
+      <button type="submit">登録</button>
+    </form>
+  );
+}
