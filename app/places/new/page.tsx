a4c6ac4be9d7caec749cf6cbe9e81958d0b1a5e3
mitsuiakane <akane.sumitomo@gmail.com>
2026-04-01 19:37:43 +0900

    feat: 投稿ページの見た目の骨組みを作成

--- app/places/new/page.tsx
+++ app/places/new/page.tsx
@@ -1,8 +1,31 @@
+import PostButton from "../../_components/PostButton";
+import Image from "next/image";
 export default function PlacesNewPage() {
   return (
-    <div>
-      <h1>新しい場所を登録</h1>
-      <p>ここに場所の登録フォームを作成します</p>
+    <div className="flex">
+      <div className="w-2/5 bg-[#020817] h-[600px] p-8">
+        <h1 className="text-white text-2xl font-bold mb-4">聖地投稿フォーム</h1>
+        <p className="text-white">タイトル</p>
+        <input type="text" className="mr-6  sm:mr-0  text-black bg-white" />
+        <p className="text-white">説明</p>
+        <input
+          type="text"
+          className="mr-6 sm:mr-0  text-black bg-white h-[100px]"
+        />
+        <p className="text-white">住所</p>
+        <input type="text" className="mr-6  sm:mr-0  text-black bg-white" />
+        <p>
+          <PostButton />
+        </p>
+      </div>
+      <div className="w-3/5 bg-pink-100 h-[600px] relative">
+        <Image
+          className=" object-cover object-center"
+          src="/map.png"
+          alt="地図画像"
+          fill
+        />
+      </div>
     </div>
-  )
+  );
 }
