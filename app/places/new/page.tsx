99c32c7d586f7ae532cc03a9eef00294c73000e3
mitsuiakane <akane.sumitomo@gmail.com>
2026-04-01 23:28:37 +0900

    feat: 聖地投稿フォームの入力管理を追加

--- app/places/new/page.tsx
+++ app/places/new/page.tsx
@@ -1,19 +1,28 @@
+'use client'
 import PostButton from "../../_components/PostButton";
 import Image from "next/image";
+import { useForm } from "react-hook-form"
+type FormValues = {
+    title: string
+    explanation: string
+    address: string
+}
 export default function PlacesNewPage() {
+  const { register, handleSubmit} = useForm<FormValues>({})
   return (
     <div className="flex">
       <div className="w-2/5 bg-[#020817] h-[600px] p-8">
         <h1 className="text-white text-2xl font-bold mb-4">聖地投稿フォーム</h1>
         <p className="text-white">タイトル</p>
-        <input type="text" className="mr-6  sm:mr-0  text-black bg-white" />
+        <input type="text" {...register('title')} className="mr-6  sm:mr-0  text-black bg-white" />
         <p className="text-white">説明</p>
         <input
           type="text"
+          {...register('explanation')}
           className="mr-6 sm:mr-0  text-black bg-white h-[100px]"
         />
         <p className="text-white">住所</p>
-        <input type="text" className="mr-6  sm:mr-0  text-black bg-white" />
+        <input type="text" {...register('address')} className="mr-6  sm:mr-0  text-black bg-white" />
         <p>
           <PostButton />
         </p>
