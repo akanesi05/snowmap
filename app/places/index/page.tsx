9d1ee06dd52919baae6ff6babfa3808fb1c4bc17
mitsuiakane <akane.sumitomo@gmail.com>
2026-06-11 00:17:27 +0900

    feat: 一覧ページの編集ボタンをログイン時のみ表示する

--- app/places/index/page.tsx
+++ app/places/index/page.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import PlacesIndexContainer from './_components/PlacesIndexContainer'
+import { auth } from "@/lib/auth";
 
 type PostWithLocation = {
   id: string
@@ -13,10 +14,11 @@ type PostWithLocation = {
 }
 
 export default async function PlacesIndexPage() {
+    const session = await auth();
     const posts = await prisma.sanctuaries.findMany();
     const mapPosts = posts.filter((post) : post is PostWithLocation => { return (post.latitude !==null && post.longitude!==null)});
     return (
     
-       <PlacesIndexContainer posts={mapPosts} />
+       <PlacesIndexContainer posts={mapPosts} session={session}/>
   )
 }
