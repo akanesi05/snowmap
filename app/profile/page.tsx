b99c5b509bd34423c125f3e8b0e0090713701f13
mitsuiakane <akane.sumitomo@gmail.com>
2026-07-11 12:54:32 +0900

    feat: プロフィールページにユーザー情報を表示

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,17 @@
-"use client";
-import { useRouter } from "next/navigation";
+
+import { auth } from "@/lib/auth";
 
 
-export default function profile() {
+export default async function profile() {
+  const session = await auth();
   
 
   return(
-  <p>プロフページです</p>)
+<>
+  <p>プロフページです</p>
+  <p>{session.user.name}</p>
+  <p>{session.user.email}</p>
+  </>
+)
 
 }
